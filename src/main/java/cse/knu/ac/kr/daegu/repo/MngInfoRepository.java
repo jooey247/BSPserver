c583ead6cb3f045d765f73cdd842199a131d320e
zzeulki <Zzeulki@gangseulgiui-MacBook-Air.local>
2016-08-05 00:30:35 +0900

    server 3차

--- src/main/java/cse/knu/ac/kr/daegu/repo/MngInfoRepository.java
+++ src/main/java/cse/knu/ac/kr/daegu/repo/MngInfoRepository.java
@@ -3,7 +3,7 @@ package cse.knu.ac.kr.daegu.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cse.knu.ac.kr.daegu.Info.MngInfo;
-import cse.knu.ac.kr.daegu.Info.PlcInfo;
+//import cse.knu.ac.kr.daegu.Info.PlcInfo;
 
 public interface MngInfoRepository extends JpaRepository<MngInfo, String>{
 	
